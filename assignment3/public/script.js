5e09ad9bd5df31897860d02a467e869589ca3b7a
Keri Hallau <khallau@montana.edu>
2015-10-05 22:56:17 -0600

    Updated script.js
    Now that I can see the scripted version in localhost i’m feeling
    better, but exhausted so i’ll have to work on it more tomorrow.  it
    still doesn’t work, but i think i’ve made some progress.

--- assignment3/public/script.js
+++ assignment3/public/script.js
@@ -2,20 +2,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     // get all of our list items
     // 1. Select all the items to filter
-    var dataList = document.getElementById('datalist');
+    var dataList = document.getElementById('listData');
 	
     //setup click event handlers on our checkboxes
     // 2. Select all the checkboxes to watch
-    var checkBoxes = ['one', 'two', 'three'];
+    var checkBoxes = document.querySelectorAll('input[type="checkbox"]');
 
-    
-			//this loops through the array
 	for (var i = 0; i < checkBoxes.length; i++) {
         checkBoxes[i].checked = true;
         // 3. Add an event listener for each checkbox
         // each checkbox should use filterItems()
-
-    }
+		
+		checkBoxes.addEventListener('onchange', filterItems); 
+	}
     // event handler
     function filterItems() {
         if (this.checked) {
@@ -28,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
         for (var i = 0; i < dataList.length; i++) {
             var item = dataList[i];
             // 4. show or hide the item if the item attribute "data-category" == value
-            if() {
+            if(document.querySelectorAll('li.data-category') == this.value) {
                 if(show){
                     item.style.display = "block";
                 }else{
